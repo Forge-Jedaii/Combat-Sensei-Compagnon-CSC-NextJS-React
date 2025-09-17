d6c4e6520053c144980dee537f099ba9b5db5e9f
Forge-Jedaii <forgejedaii@gmail.com>
2025-09-17 16:06:31 +0200

    ajout mongoose et lib/mongodb.ts

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI || "";
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  // Déclare une propriété mongoose sur global
+  var mongoose: MongooseCache | undefined;
+}
+
+const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  global.mongoose = cached; // sauvegarde dans global
+  return cached.conn;
+}
+
+export default connectToDatabase;
