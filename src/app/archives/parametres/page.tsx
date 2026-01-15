34ca6b6476170b4cf4fc27a5ee376c3ce72dad4f
Forge-Jedaii <forgejedaii@gmail.com>
2026-01-15 13:32:52 +0100

    page parametres

---
+++ src/app/archives/parametres/page.tsx
@@ -0,0 +1,8 @@
+export default function ParametresPage() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-2xl font-bold">Paramètres</h1>
+      <p className="opacity-70">Page en cours de construction</p>
+    </div>
+  );
+}
